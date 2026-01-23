d70b2a0f1bbe27da22e8c24b907433a993dad201
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-01-23 13:46:53 -0700

    feat: Add milvus setup

---
+++ milvus/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+# Set limiton number of open files
+ulimit -n 1048576
+
+# Set max number of vmas
+sudo sysctl -w vm.max_map_count=262144
+
+# Set max number of connections
+sudo sysctl -w net.core.somaxconn=4096
+
+# CPU performance mode
+sudo apt update
+sudo apt install -y linux-tools-common linux-tools-$(uname -r)
+sudo cpupower frequency-set -g performance || true
+
+# Install docker
+sudo apt install -y docker.io docker-compose
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Milvus standalone
+wget https://github.com/milvus-io/milvus/releases/download/v2.3.21/milvus-standalone-docker-compose.yml \
+  -O docker-compose.yml
+
+sudo docker-compose up -d
+
