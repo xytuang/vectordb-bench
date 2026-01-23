a782ff7d4f8429f3c7e06251b29228fa8d41ccc0
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-01-23 13:03:01 -0700

    feat: Add minio setup script

---
+++ minio/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Install go
+wget https://go.dev/dl/go1.25.6.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.25.6.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> $HOME/.profile
+source $HOME./profile
+
+# Install and start docker
+sudo apt update
+sudo apt install -y docker.io
+sudo systemctl enable docker
+sudo systemctl start docker
+
+mkdir -p minio-data
+
+sudo docker run -d \
+  --name minio \
+  -p 9000:9000 \
+  -p 9001:9001 \
+  -v /mydata/minio-data:/data \
+  -e MINIO_ROOT_USER=minio \
+  -e MINIO_ROOT_PASSWORD=minio123 \
+  minio/minio server /data --console-address ":9001"
+
+
+
+
+
+
+
+
+
