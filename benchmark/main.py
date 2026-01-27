a043b8ef53ac18e89ab5975d27349e563c8ce72d
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-01-27 12:24:02 -0700

    feat: Update benchmarking setup

--- benchmark/main.py
+++ benchmark/main.py
@@ -1,5 +1,25 @@
 from pymilvus import connections
-connections.connect("default", host="node1", port="19530")
-print("Connected to Milvus successfully!")
-connections.disconnect("default")
+import subprocess
+
+def start():
+    connections.connect("default", host="node1", port="19530")
+    print("Connected to Milvus successfully!")
+
+def get_dataset():
+    try:
+        subprocess.run(["./fetch_data.sh"], check=True)
+        print("Fetched data sucessfully")
+    except subprocess.CalledProcessError as e:
+        print(f"Script failed with return code: {e.returncode}")
+    except FileNotFoundError:
+        print(f"Error: The script file was not found.")
+
+
+def stop():
+    connections.disconnect("default")
+
+if __name__ == "__main__":
+    start()
+    get_dataset()
+    stop()
 
