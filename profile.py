f48973843fb2bbb5146da7d331f4995946ce466a
Xiang Yu Tuang <xiangyutuang@gmail.com>
2026-02-03 21:43:36 -0700

    feat: Update profile

--- profile.py
+++ profile.py
@@ -24,7 +24,7 @@ bs0 = node0.Blockstore("bs0", "/mydata")
 
 # Milvus node - memory focused  
 node1 = request.RawPC("node1")
-node1.hardware_type = "c6525-25g"  # or "c6525-100g" or "d7615" or "xl170" for more RAM
+node1.hardware_type = "d7615"  # or "c6525-100g" or "d7615" or "xl170" for more RAM
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD"
 bs1 = node1.Blockstore("bs1", "/mydata")
 
