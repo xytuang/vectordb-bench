7ccc74ae74cb2685aa0163511861f91cb86c52d4
xytuang <88203777+xytuang@users.noreply.github.com>
2026-02-03 23:46:00 -0500

    Update hardware type for node0 and node1

--- profile.py
+++ profile.py
@@ -17,14 +17,14 @@ request = pc.makeRequestRSpec()
 
 # MinIO node - storage focused
 node0 = request.RawPC("node0")
-node0.hardware_type = "d750"  # Use the Optane SSD on the d750 or "c6525-100g" or "xl170"
+node0.hardware_type = "d7615"  # or "c6525-100g" or "d7615" for better SSD
 node0.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD"
 bs0 = node0.Blockstore("bs0", "/mydata")
 
 
 # Milvus node - memory focused  
 node1 = request.RawPC("node1")
-node1.hardware_type = "d7615"  # or "c6525-100g" or "d7615" or "xl170" for more RAM
+node1.hardware_type = "d7615"  # Use the Optane SSD on the d750 or "c6525-100g" or "xl170" 
 node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD"
 bs1 = node1.Blockstore("bs1", "/mydata")
 
