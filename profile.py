b55eb4366688705c3be79af8eadabe87290167d2
xytuang <88203777+xytuang@users.noreply.github.com>
2026-03-29 23:00:03 -0400

    Update disk image for node0 to Ubuntu 22.04

--- profile.py
+++ profile.py
@@ -18,7 +18,7 @@ request = pc.makeRequestRSpec()
 # MinIO node - storage focused
 node0 = request.RawPC("node0")
 node0.hardware_type = "m400"  # or "c6525-100g" or "d750" or "c6525-25g" for better SSD
-node0.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD"
+node0.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-ARM"
 bs0 = node0.Blockstore("bs0", "/mydata")
 
 node1 = request.RawPC("node1")
