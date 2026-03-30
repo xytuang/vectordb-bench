1d925b1cba68128fb95bb27a0e3b4afd7a407dd0
xytuang <88203777+xytuang@users.noreply.github.com>
2026-03-29 21:56:47 -0400

    Change hardware type from c6525-25g to xl170

--- profile.py
+++ profile.py
@@ -17,7 +17,7 @@ request = pc.makeRequestRSpec()
 
 # MinIO node - storage focused
 node0 = request.RawPC("node0")
-node0.hardware_type = "c6525-25g"  # or "c6525-100g" or "d750" or "c6525-25g" for better SSD
+node0.hardware_type = "xl170"  # or "c6525-100g" or "d750" or "c6525-25g" for better SSD
 node0.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU24-64-STD"
 bs0 = node0.Blockstore("bs0", "/mydata")
 
